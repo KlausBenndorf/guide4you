c1d6bf8c4a8299f3c6f723c501b07350b18d31e1
Simon Seyock <simonseyock@gmail.com>
2016-08-09 16:48:31 +0200

    Make repository travis CI compatible

--- tests/selenium/customPhantomDriver.js
+++ tests/selenium/customPhantomDriver.js
@@ -1,6 +1,8 @@
 import webdriver from 'selenium-webdriver'
 import phantomjs from 'phantomjs-prebuilt'
 
+console.log('using phantomjs version: ' + phantomjs.version)
+
 let customPhantom = webdriver.Capabilities.phantomjs()
 customPhantom.set('phantomjs.binary.path', phantomjs.path)
 
