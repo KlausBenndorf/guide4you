fe5d32523a158dc2c033c0b5d45aedc3936be265
Simon Seyock <simonseyock@gmail.com>
2016-08-09 10:56:57 +0200

    phantomjs for selenium tests

---
+++ tests/selenium/customPhantomDriver.js
@@ -0,0 +1,9 @@
+import webdriver from 'selenium-webdriver'
+import phantomjs from 'phantomjs-prebuilt'
+
+let customPhantom = webdriver.Capabilities.phantomjs()
+customPhantom.set('phantomjs.binary.path', phantomjs.path)
+
+export default function () {
+  return new webdriver.Builder().withCapabilities(customPhantom).build()
+}
