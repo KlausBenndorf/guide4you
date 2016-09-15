dca433327d0b27e24040e95585d69a712cd2fa9d
Simon Seyock <simonseyock@gmail.com>
2016-09-15 18:12:23 +0200

    removed l10n.json.js

--- files/l10n.json.js
+++
@@ -1,5 +0,0 @@
-// As the webpack loader's exec method is broken we need to do plain ES5 here
-
-var merge = require('lodash/merge')
-
-module.exports = merge(require('guide4you/files/l10n.json.js'), require('guide4you-module-search/files/l10n.json.js'))
