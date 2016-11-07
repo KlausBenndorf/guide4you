7d58ae7cc0c63ef693f62ac40602a5e899b05eaa
Josef Schugt <josef.schugt@benndorf.de>
2016-11-07 09:24:06 +0000

    helptext.json.js cleanup

--- files/helptext.json.js
+++ files/helptext.json.js
@@ -1,7 +1,5 @@
 // As the webpack loader's exec method is broken we need to do plain ES5 here
 
-// As the webpack loader's exec method is broken we need to do plain ES5 here
-
 var merge = require('lodash/merge')
 
 module.exports = merge(require('guide4you/files/helptext.json.js'), {
