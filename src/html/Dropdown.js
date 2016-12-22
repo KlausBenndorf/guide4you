8043faa488311e8b40e0ca4faa99f4db8177df80
Simon Seyock <simonseyock@gmail.com>
2016-12-22 14:42:21 +0100

    IScroll related fixes. Added Polyfill.

--- src/html/Dropdown.js
+++ src/html/Dropdown.js
@@ -2,6 +2,8 @@ import ol from 'openlayers'
 import $ from 'jquery'
 import { cssClasses, keyCodes } from '../globals'
 
+import 'polyfill!Array.prototype.findIndex'
+
 import '../../less/dropdown.less'
 
 /**
