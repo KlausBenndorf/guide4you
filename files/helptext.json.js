85f379b00bd6edebccc4136c311cd6d15f51689d
Josef Schugt <josef.schugt@benndorf.de>
2017-01-19 15:26:12 +0100

    helptext using objects, not array

--- files/helptext.json.js
+++ files/helptext.json.js
@@ -3,19 +3,17 @@
 var merge = require('lodash/merge')
 
 module.exports = merge(require('guide4you/files/helptext.json.js'), {
-  "de": [
-    {
-      "id": "linkGeneratorButton",
-      "img": "button-link.png",
-      "descr": "Mit dieser Schalt&shy;fl&auml;che zeigen Sie einen Link (Verweis) auf die aktuell dargestellte Karten&shy;ansicht an."
+  "de": {
+    "linkGeneratorButton": {
+      "img": ["button-link.png"],
+      "descr": ["Mit dieser Schalt&shy;fl&auml;che zeigen Sie einen Link (Verweis) auf die aktuell dargestellte Karten&shy;ansicht an."]
     }
-  ],
+  },
   //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-  "en": [
-    {
-      "id": "linkGeneratorButton",
-      "img": "button-link.png",
-      "descr": "Use this button to display a link to the current view of the map."
+  "en": {
+    "linkGeneratorButton": {
+      "img": ["button-link.png"],
+      "descr": ["Use this button to display a link to the current view of the map."]
     }
-  ]
+  }
 })
