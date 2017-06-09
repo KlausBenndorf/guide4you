f285b62a1a8ae2f060ea74a3a27f73fe395dc056
Simon Seyock <simonseyock@gmail.com>
2017-06-09 16:02:46 +0200

    adding exports.js and simplifying imports in less

--- conf/simple/entry.js
+++ conf/simple/entry.js
@@ -1,4 +1,4 @@
-import { createG4UInternal } from '../../src/main'
+import {createMapInternal} from '../../src/main'
 
 import defaultClientConf from 'mustache-eval-loader?name=conf/[name].[ext]!./client.commented.json'
 import defaultLayerConf from 'mustache-eval-loader?name=conf/[name].[ext]!./layers.commented.json'
@@ -9,5 +9,7 @@ import 'file-loader?name=files/[name].[ext]!../../files/hotelsbonn.kml'
 import 'file-loader?name=files/[name].[ext]!../../files/restaurantsbonn.kml'
 
 export function createMap (target, clientConf = defaultClientConf, layerConf = defaultLayerConf) {
-  return createG4UInternal(target, clientConf, layerConf)
+  return createMapInternal(target, clientConf, layerConf)
 }
+
+export * from '../../src/exports'
