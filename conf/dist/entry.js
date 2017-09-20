ed7794c0bb94e16d27ecd9b87464398667172b5e
Simon Seyock <simonseyock@gmail.com>
2017-09-20 13:54:18 +0200

    mustache-loader

--- conf/dist/entry.js
+++ conf/dist/entry.js
@@ -2,7 +2,7 @@ import 'file-loader?name=[name].[ext]!./g4u.d.ts'
 
 import {createMapInternal} from '../../src/main'
 
-import 'mustache-eval-loader?name=conf/[name].[ext]!../full/client.commented.json'
+import 'file-loader?name=conf/[name].[ext]!mustache-loader!../full/client.commented.json'
 import 'file-loader?name=conf/[name].[ext]!./layers.commented.json'
 
 import 'tojson-file-loader?name=files/[name]!../../files/l10n.json.js'
--- conf/simple/webpack.js
+++ conf/simple/webpack.js
@@ -1,14 +1,14 @@
 'use strict'
 
 const HtmlWebpackPlugin = require('html-webpack-plugin')
-const mustacheEvalLoader = require('guide4you-builder/mustache-eval-loader')
+const mustacheLoader = require('guide4you-builder/mustache-loader')
 const HtmlWebpackIncludeAssetsPlugin = require('html-webpack-include-assets-plugin')
 const CopyWebpackPlugin = require('copy-webpack-plugin')
 
 let path = require('path')
 let baseDir = process.cwd()
 
-mustacheEvalLoader.setTemplateVars({
+mustacheLoader.setTemplateVars({
   ajaxProxy: {
     prod: 'proxy/proxy.php?csurl={url}',
     dev: '/proxy/{url}'
