0343f8c5aeec268d3a2c1a8605d74f790e124936
Simon Seyock <simonseyock@gmail.com>
2018-12-03 16:48:20 +0100

    onavailable for properties

--- src/openlayersInjections.js
+++ src/openlayersInjections.js
@@ -22,3 +22,25 @@ ol.Object.prototype.asSoonAs = function (propName, value, cb) {
     })
   }
 }
+
+/**
+ * extends the openlayers ol.Object class. Waits till a property is set to a specific value and calls the given
+ * callback function. If the value already has the desired value the callback is called immediately.
+ * @param {string} propName
+ * @param {*} value
+ * @param {function} cb
+ */
+ol.Object.prototype.onAvailable = function (propName, cb) {
+  if (!$.isFunction(cb)) {
+    throw new Error('You need to provide a callback to asSoonAs.')
+  }
+  if (this.get(propName) !== undefined) {
+    // run callback
+    cb()
+  } else {
+    this.once('change:' + propName, () => {
+      // recursive call
+      this.onAvailable(propName, cb)
+    })
+  }
+}
